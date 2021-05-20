bb84584174138512c7f8ccddeeaa4000aae96ca5
ikebo <k_1043@126.com>
2021-05-20 14:41:20 +0800

    init

---
+++ Tinyhttpd/simpleclient.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char ch = 'A';
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = htons(9734);
+    len = sizeof(address);
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    // stdout 默认是行缓冲 ref: 
+    printf("result: %d\n", result);
+    if (result == -1)
+    {
+        perror("oops: client1");
+        exit(1);
+    }
+    printf("before write\n");
+    write(sockfd, &ch, 1);
+    printf("write ok..\n");
+    // read这里阻塞了，一个字节的数据，对端再等一个完整的http请求，所以没有处理
+    read(sockfd, &ch, 1);
+    printf("char from server = %c\n", ch);
+    close(sockfd);
+    exit(0);
+}
